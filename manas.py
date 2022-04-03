a9a61aec04596204b4147129b3c9c7637273f2b0
manasbobade2023 <manasbobade2023@gmail.com>
2022-04-03 11:22:38 +0530

    3rd commit

--- manas.py
+++ manas.py
@@ -2,3 +2,13 @@ a=int(input("enter the number"))
 
 print("manas=",a)
 print("change 2")
+
+"""
+git status
+git --version
+git init
+git add
+git commit -m ""
+git remote add origin 
+git push -u origin master
+"""
