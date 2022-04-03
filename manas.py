c75714cb884fb4a1b80ab93b33273495aa5ee820
manasbobade2023 <manasbobade2023@gmail.com>
2022-04-03 12:39:47 +0530

    commit number5

--- manas.py
+++ manas.py
@@ -11,5 +11,7 @@ git add
 git commit -m ""
 git remote add origin 
 git push -u origin master
+git config --global user.email "manasbobade2023@gmail.com"
+git config --global user.name "manasbobade2023"   
 """
 print("manas2")
